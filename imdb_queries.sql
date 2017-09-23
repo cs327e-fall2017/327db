e28c2e51544478581bf9bc10bc06e0df1c8c760e
danieljsmarda <danieljsmarda@gmail.com>
2017-09-22 20:52:07 -0500

    Added Query #4

--- imdb_queries.sql
+++ imdb_queries.sql
@@ -10,9 +10,9 @@ WHERE tr.num_votes > 100 and tb.title_type = 'movie' and is_adult = FALSE
 ORDER BY tr.average_rating desc;
 
 /* QUERY #2 (CPY86)
-This query searches for foreign movies by checking the primary title is 
-different toits original title, since the original title uses the foreign 
-language and the primary title is the translation. It also returns the 
+This query searches for foreign movies by checking the primary title is
+different toits original title, since the original title uses the foreign
+language and the primary title is the translation. It also returns the
 director's name. All of this will be ordered by the movie's starting year.*/
 
 select tb.primary_title, pb.primary_name
@@ -22,7 +22,7 @@ and tb.title_type = 'movie'
 order by tb.start_year asc;
 
 /*QUERY #3 (CPY86)
-This query searches for people and the title they're most famous for, 
+This query searches for people and the title they're most famous for,
 where the person has already deceased. It makes sure to pass living
 people by checking if death year is null or not, and is ordered by the
 title's starting year*/
@@ -31,3 +31,14 @@ select pb.primary_name, pb.death_year, tb.primary_title
 from title_basics tb join stars s on (tb.title_id = s.title_id) join person_basics pb on (s.person_id = pb.person_id)
 where pb.death_year is NOT NULL
 order by tb.start_year asc;
+
+
+/*Query #4 (DJS3745)
+What is the oldest movie in the database
+that Tom Cruise starred in? */
+
+SELECT tb.primary_title, tb.start_year
+FROM person_basics pb JOIN stars s ON s.person_id = pb.person_id
+JOIN title_basics tb ON tb.title_id = s.title_id
+WHERE tb.title_type = 'movie'and pb.primary_name = 'Tom Cruise'
+ORDER BY tb.start_year asc;
