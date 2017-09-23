92292a273478f6f7a751809bae5663e1a70776e9
danieljsmarda <danieljsmarda@gmail.com>
2017-09-22 21:59:42 -0500

    Added Query #9

--- imdb_queries.sql
+++ imdb_queries.sql
@@ -57,7 +57,6 @@ join title_basics tb on (s.title_id = tb.title_id) join title_ratings tr on (tb.
 order by tr.average_rating desc;
 
 
-
 /* Query #6 (DJS3745)
 What movies do people hate so much they just NEED to rate it terribly?*/
 
@@ -76,8 +75,9 @@ JOIN title_basics tb ON tb.title_id = w.title_id
 WHERE pb.death_year <= 2017 and pb.death_year >= 2016 and tb.title_type = 'tvSeries'
 ORDER BY tr.average_rating desc
 
+
 /*QUERY #8 (CPY86)
-This query searches for actors that were born after 2000 and prints 
+This query searches for actors that were born after 2000 and prints
 their birth year, and orders it by actor's name*/
 
 select pb.primary_name, pb.birth_year
@@ -85,3 +85,19 @@ from person_basics pb join person_professions pp on (pb.person_id = pp.person_id
 where pb.birth_year > 2000
 and pp.profession = 'actor'
 order by pb.primary_name asc;
+
+
+/* Query #9
+I want to learn something but I don't
+have a lot of time. Show me the shortest
+good documentaries available, ideally made
+recently. I'm going to assume
+that any documentary with a length of less than
+20 minutes is an erroneous entry in the
+database. */
+
+SELECT tb.primary_title, tb.runtime_minutes, tr.average_rating, tb.start_year
+FROM title_basics tb JOIN title_genres tg ON tb.title_id = tg.title_id
+JOIN title_ratings tr ON tr.title_id = tb.title_id
+WHERE tg.genre = 'Documentary' and tb.runtime_minutes >= 20
+ORDER BY tb.runtime_minutes, tb.start_year desc, tr.average_rating desc
