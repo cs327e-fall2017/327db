60c20c4d024876d6f2d6f8d926d2622b3b78cdd1
danieljsmarda <danieljsmarda@gmail.com>
2017-09-22 20:16:00 -0500

    Added Query#1

---
+++ imdb_queries.sql
@@ -0,0 +1,10 @@
+
+/* QUERY #1
+Find me the highest rated movie in the databases,
+but make sure it's kid-friendly so that I can watch
+it with my little cousin this evening.*/
+
+SELECT tb.primary_title, tr.average_rating
+FROM title_basics tb JOIN title_ratings tr ON tb.title_id = tr.title_id
+WHERE tr.num_votes > 100 and tb.title_type = 'movie' and is_adult = FALSE
+ORDER BY tr.average_rating desc;
