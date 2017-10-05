410fe2a0fd64665bd1d78f33c800ef2b74e665b7
Changyong Peter Yi <peter.yi623@gmail.com>
2017-10-05 14:53:04 -0500

    pushed to prevent merge conflicts

--- lab2/create_base_tables.sql
+++ lab2/create_base_tables.sql
@@ -7,6 +7,8 @@ create database imdb;
 \c imdb
 */
 
+drop table if exists title_basics;
+
 create table if not exists title_basics (
   title_id char(9) primary key,
   title_types varchar(12),
@@ -18,12 +20,16 @@ create table if not exists title_basics (
   runtime_minutes int 
 );
 
+drop table if exists person_basics;
+
 create table if not exists person_basics (
   person_id char(9) primary key,
   primary_name char(105) not null,
   birth_year int,
   death_year int
 );
+
+drop table if exists title_episodes;
 
 create table if not exists title_episodes (
   title_id char(9) primary key,
@@ -32,41 +38,55 @@ create table if not exists title_episodes (
   episode_num int
 );
 
+drop table if exists title_ratings;
+
 create table if not exists title_ratings (
   title_id char(9) primary key,
   average_rating numeric,
-  num_votes int,
+  num_votes int
 );
+
+drop table if exists directors;
 
 create table if not exists directors (
   title_id char(9),
-  person_id char(9)
-  PRIMARY KEY (title_id, person_id),
-  FOREIGN KEY director2title_basics (title_id)
+  person_id char(9),
+  PRIMARY KEY (title_id, person_id)
+  FOREIGN KEY directors2title_basics (title_id)
   REFERENCES title_basics
   FOREIGN KEY directors2person_basics (person_id)
   REFERENCES person_basics
-  );
+);
+
+drop table if exists writers;
 
 create table if not exists writers (
   title_id char(9),
-  person_id char(9) 
+  person_id char(9)
 );
+
+drop table if exists principals;
 
 create table if not exists principals (
   title_id char(9),
   person_id char(9)
 );
 
+drop table if exists stars;
+
 create table if not exists stars (
   person_id char(9),
   title_id char(9)
 );
 
+drop table if exists person_professions;
+
 create table if not exists person_professions (
   person_id char(9),
   professions varchar(25)
 );
+
+drop table if exists title_genres;
 
 create table if not exists title_genres (
   title_id char(9),
