296bc4442775c885fb44c7177223b2d0e60295b4
Changyong Peter Yi <peter.yi623@gmail.com>
2017-10-05 13:44:58 -0500

    added constraints and added one new table to imdb

--- lab2/create_base_tables.sql
+++ lab2/create_base_tables.sql
@@ -10,17 +10,24 @@ create database imdb;
 create table if not exists title_basics (
   title_id char(9) primary key,
   title_types varchar(12),
-  primary_title varchar(292),
+  primary_title varchar(292) not null,
   original_title varchar(292),
   is_adult boolean,
-    start_year int,
-    end_year int,
-    runtime_minutes int 
+  start_year int,
+  end_year int,
+  runtime_minutes int 
 );
 
 create table if not exists person_basics (
   person_id char(9) primary key,
-  primary_name char(105),
+  primary_name char(105) not null,
   birth_year int,
   death_year int
 );
+
+create table if not exists title_episodes (
+  title_id char(9) primary key,
+  parent_title char(9),
+  season_num int,
+  episode_num int,
+)
