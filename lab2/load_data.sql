be4e6a6f666c18ec43c4a80bfcf66ada7bb30803
Changyong Peter Yi <peter.yi623@gmail.com>
2017-10-06 19:54:20 -0500

    edit load_data.sql to include copy commands for the split pricipals table

--- lab2/load_data.sql
+++ lab2/load_data.sql
@@ -1,6 +1,6 @@
 \c imdb;
 
-\copy title_basics from C:/Users/Peter/Documents/FALL2017/Database/pg/title_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+/*\copy title_basics from C:/Users/Peter/Documents/FALL2017/Database/pg/title_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
 \copy person_basics from C:/Users/Peter/Documents/FALL2017/Database/pg/person_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
@@ -12,8 +12,32 @@
 
 \copy writers from C:/Users/Peter/Documents/FALL2017/Database/pg/writers.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from C:/Users/Peter/Documents/FALL2017/Database/pg/principals.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xaa' (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xab' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xac' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xad' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xae' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xaf' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xag' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xah' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xai' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xaj' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xak' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xal' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+
+\copy principals from 'C:/Users/Peter/Documents/FALL2017/Database/split_principals/xam' (header FALSE, format csv, delimiter ',', null '', encoding 'UTF8');
+*/
 \copy stars from C:/Users/Peter/Documents/FALL2017/Database/pg/stars.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
 \copy person_professions from C:/Users/Peter/Documents/FALL2017/Database/pg/person_professions.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
