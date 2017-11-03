d55c1743f66903bab07cc6a164b4a3b20e54d5a1
Changyong Peter Yi <peter.yi623@gmail.com>
2017-11-03 10:37:33 -0500

    updated movielens_ratings.out and modified v_movielens_rating.sql

--- final-project/v_movielens_rating.sql
+++ final-project/v_movielens_rating.sql
@@ -1,6 +1,6 @@
 /* Displays difference between imdb rating and movielens rating for each movie genre */
-SELECT tg.genre, tr.average_rating, tr.movielens_rating, ABS(tr.average_rating-tr.movielens_rating) AS rating_difference
-FROM title_genres tg JOIN title_rating tr ON tg.title_id = tr.title_id
+SELECT tg.genre, tr.average_rating, tr.movielens_rating*2, ABS(tr.average_rating-tr.movielens_rating*2) AS rating_difference
+FROM title_genres tg JOIN title_ratings tr ON tg.title_id = tr.title_id
 WHERE tr.movielens_rating IS NOT NULL
 ORDER BY tg.genre;
 
