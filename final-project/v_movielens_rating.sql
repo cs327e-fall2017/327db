6e91e78c0e9cf3b2e50c79cbc87151e1d0ca8e5c
Changyong Peter Yi <peter.yi623@gmail.com>
2017-11-03 18:17:49 -0500

    added image file of quicksight and modified v_movielens_rating.sql

--- final-project/v_movielens_rating.sql
+++ final-project/v_movielens_rating.sql
@@ -1,6 +1,8 @@
 /* Displays difference between imdb rating and movielens rating for each movie genre */
-SELECT tg.genre, tr.average_rating, tr.movielens_rating*2, ABS(tr.average_rating-tr.movielens_rating*2) AS rating_difference
-FROM title_genres tg JOIN title_ratings tr ON tg.title_id = tr.title_id
-WHERE tr.movielens_rating IS NOT NULL
-ORDER BY tg.genre;
+CREATE VIEW v_movielens_rating AS
+	SELECT tg.genre, AVG(ABS(tr.average_rating-tr.movielens_rating*2)) AS rating_difference
+	FROM title_genres tg JOIN title_ratings tr ON tg.title_id = tr.title_id
+	WHERE tr.movielens_rating IS NOT NULL
+	GROUP BY tg.genre
+	ORDER BY tg.genre;
 
