41b5ecfd4c5d8a3224c115a6559110bfb55f63f9
Changyong Peter Yi <peter.yi623@gmail.com>
2017-11-17 16:22:24 -0600

    edited v_title_financial.sql

--- final-project/v_title_financials.sql
+++ final-project/v_title_financials.sql
@@ -1,4 +1,5 @@
-SELECT tb.title_id, tf.budget, tf.box_office, tr.title_ratings
-FROM title_ratings tr JOIN title_basics tb ON tb.title_id=tr.title_id
-JOIN Title_Financials tf ON tf.title_id = tb.title_id
-WHERE tf.budget > 1000000;
+CREATE VIEW v_title_financials AS
+	SELECT tb.title_id, tf.budget, tf.box_office, tr.average_rating
+	FROM title_ratings tr JOIN title_basics tb ON tb.title_id=tr.title_id
+	JOIN Title_Financials tf ON tf.title_id = tb.title_id
+	WHERE tf.budget > 1000000;
