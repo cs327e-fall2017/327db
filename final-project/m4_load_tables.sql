6de01be89c9a929325548f648c9ad00c91381701
Daniel Smarda <danieljsmarda@gmail.com>
2017-11-28 20:55:16 -0600

    Delete m4_load_tables.sql

--- final-project/m4_load_tables.sql
+++
@@ -1,7 +0,0 @@
-\copy songs from C:/Users/Peter/Documents/FALL2017/Database/cinemalytics-with-headers/songs.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
-
-\copy temp_singer_songs from C:/Users/Peter/Documents/FALL2017/Database/cinemalytics-with-headers/singer_songs.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
-
-\copy temp_persons from C:/Users/Peter/Documents/FALL2017/Database/cinemalytics-with-headers/persons.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
-
-insert into singer_songs select * from temp_singer_songs where person_id IN (select person_id from temp_persons);
