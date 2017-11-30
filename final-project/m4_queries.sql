206f5f8ae2c4c2c276ef9905bf535a71ec426701
danieljsmarda <danieljsmarda@gmail.com>
2017-11-29 20:44:06 -0600

    Added second m4 query

--- final-project/m4_queries.sql
+++ final-project/m4_queries.sql
@@ -4,3 +4,12 @@ CREATE VIEW v_year_song_duration AS
 	FROM title_basics tb JOIN title_songs ts ON tb.title_id = ts.title_id 
 	JOIN songs s ON ts.song_id = s.song_id
 	ORDER BY tb.start_year desc;
+	
+
+/* How many minutes of total music are in the database by gender?
+Is there gender bias? */
+CREATE VIEW v_gender_song_lengths AS
+	SELECT pb.gender, SUM(s.song_duration)
+	FROM person_basics pb JOIN singer_songs ss ON pb.person_id=ss.person_id
+	JOIN songs s ON s.song_id=ss.song_id
+	GROUP BY pb.gender;
